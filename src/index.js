8485df147a42fd14309d7548188b89c1b8835be6
md2 <md2@bellcode.com>
2019-04-15 15:33:07 +0800

    :feat: 增加导出 json 与 xml 互转的库

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const astToBlockJson = require("./ast-to-block-json")
 const astToPy = require("./ast-to-py")
 const blockJsonToAst = require("./block-json-to-ast")
+const xmlToJson = require("./xml-to-json")
+const jsonToXml = require("./json-to-xml")
 
-module.exports = { astToBlockJson, astToPy, blockJsonToAst }
+module.exports = {
+  astToBlockJson,
+  astToPy,
+  blockJsonToAst,
+  xmlToJson,
+  jsonToXml
+}

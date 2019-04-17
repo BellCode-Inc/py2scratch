51db9d8f3369a58ba709ae1fe749c000d14e1eae
md2 <md2@bellcode.com>
2019-04-17 10:48:49 +0800

    :feat: 把 flibert 导出，避免重复依赖

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const astToPy = require("./ast-to-py")
 const blockJsonToAst = require("./block-json-to-ast")
 const xmlToJson = require("./xml-to-json")
 const jsonToXml = require("./json-to-xml")
+const { parse } = require("filbert")
 
 module.exports = {
   astToBlockJson,
   astToPy,
   blockJsonToAst,
   xmlToJson,
-  jsonToXml
+  jsonToXml,
+  parsePyToAst: parse
 }
